00941960dcf8f69185ebce60b986c2e9939539a4
Arnau <agomez1f@gmail.com>
2021-01-08 20:00:10 +0100

    Initial Commit

---
+++ Joves Lectors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Joves Lectors
+//
+//  Created by Arnau on 08/01/2021.
+//  Copyright © 2021 Associació de Joves Lectors Catalans. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
